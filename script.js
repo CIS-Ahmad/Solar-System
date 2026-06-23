6b34e275a3fc016aaa0415a6d4bf7541a293c42f
ICE <152382318+CIS-Ahmad@users.noreply.github.com>
2026-06-23 13:58:35 +0300

    Fix display style for 'None' option in PlanetsList

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ calculateBtn.addEventListener("click", () => {
 PlanetsList.addEventListener("input", () => {
 
     if (PlanetsList.value !== 'None') {
-        GETElemntUsingID("None").style.display = "none";
+        GETElemntUsingID("None").style.display = "";
     }
     Main();
 
